fe93607e03941ee693105093ca4cd94644d15626
arturo <resendiz.galleta@gmail.com>
2021-05-27 21:52:39 -0500

    falta el portafolio

---
+++ assets/js/carousel.js
@@ -0,0 +1,90 @@
+$('.productos').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  autoplaySpeed: 1000,
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+$('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    autoplaySpeed: 1000,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+$('.imagenes').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    autoplaySpeed: 1000,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
