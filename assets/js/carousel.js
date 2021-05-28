979dc36237da6eec4d396a789d5d1ac62e8658b6
arturo <resendiz.galleta@gmail.com>
2021-05-28 00:58:47 -0500

    aun falta °°

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -27,6 +27,35 @@ $('.productos').slick({
   ]
 });
 
+$('.soporte').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  autoplaySpeed: 1000,
+  autoplay: true,
+  infinite: true,
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
 
 
 $('.carousel').slick({
