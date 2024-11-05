bef7c4ec1c2207fa79cc719b5f5ab1e089a183e8
Arturo Resendiz <resendiz.galleta@gmail.com>
2024-11-05 17:23:15 -0600

    Primer commit, o segundo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,42 +3,131 @@
 @section('title', 'Inicio')
 
 <div class="container-fluid">
-    <div class="row" style="background-color: rgb(71, 80, 205)">
+    <div class="row" id="animacion1" style="background-color: rgb(46, 115, 205)">
 
-        <div class="col-6 text-center p-5">
-            <img src="img/logo10.png" class="img-fluid w-25" alt="">
+        <div class="col-sm-12 col-md-6 col-lg-5 text-center p-5">
+            <img src="img/logo10.png" class="img-fluid w-sm-100 w-25" alt="">
         </div>
 
-        <div class="col-6 text-center text-white p-5 ">
+        <div class="col-sm-12 col-md-6 col-lg-7 text-center text-white p-5 ">
             <span class="titulo fw-bolder">Marketing y Técnologia</span> <br>
-            <small class="fs-5">Tehuacan, Puebla</small>
+            <span class="fs-3">Tehuacan, Puebla
+                <a class="text-white direccion" href="https://maps.app.goo.gl/ZbcZq7pZrr25fq4W8" target="_blank">Fraccionamiento: Vista Natura, Número 5121.</a>     
+            </span> <br>
+            <span class="fs-5 fw-bold"> 
+                <a class="text-white direccion" href="tel:+522211959921">Télefono:  221 195 99 21</a> 
+            </span>
         </div>
 
     </div>
 
 
-    <div class="row banner text-white p-5">
 
 
-        <div class="col-6 p-5 text-center text-with-border  bolder">
-            <span class="fw-bolder">DESARROLLO DE PÁGINAS Y APLICACIONES WEB</span> <br>
+    <div class="row banner justify-content-around text-white py-5 my-1">
+
+        <div class="col-sm-12 col-md-12 col-lg-7 p-5 bolder text-sm-center text-lg-start text-md-start bg-dark box hidden animate__animated animate__backInDown " style="--bs-bg-opacity: .7;">
+            <span class="fw-bolder text-with-border">PÁGINAS WEB Y APLICACIONES WEB</span> <br>
+            {{-- <ul>
+                <li class="text-with-border-media">Página web para tu negocio, ofrece tus productos y servicios por internet</li>
+                <li class="text-with-border-media">Invitaciones web para todos tus eventos (Bodas, Bautizos, XV Años).</li>
+                <li class="text-with-border-media">Un sitio web para tu Escuela.</li>
+            </ul> --}}
         </div>
 
+        <div class="col-sm-12 col-md-12 col-lg-1  bolder text-sm-center text-lg-start text-md-start mt-5 p-5">
+            <button class="btn btn-info btn-lg p-5 d-flex fw-bold animate__animated fs-2 animate__rubberBand mt-3">
+                <i class="fa fa-money"></i>
+                Cotizar
+            </button>
+        </div>
+
+    </div>
 
 
-        <div class="col-6 p-5 bolder">
-            <span class="fw-bolder text-with-border">PÁGINAS WEB</span> <br>
 
-            <ul>
-                <li>Publicidad</li>
-                <li>Invitaciones Web</li>
+
+
+    <div class="row p-4 py-5 justify-content-center banner1">
+
+        <div class="col-sm-12 col-md-12 col-lg-3 border py-5 px-4 mx-3 my-5 bg-white shadow shadows-sm rounded " >
+            <h3 class="text-center fw-bold">Invitaciones WEB</h3>
+            <hr>
+            <span class="fs-5 text-justify">Las invitaciones a tus eventos en una página web.</span>
+            <ul class="mt-5 great-vibes-regular fs-2">
+                <li> Boda </li>
+                <li> Cumpleaños </li>
+                <li> Bautizos </li>
+                <li> Eventos Sociales </li>
             </ul>
-
-
-
+            guadalupe_galeana24
+            Yehualtepec10
         </div>
 
+
+        <div class="col-sm-12 col-md-12  col-lg-3 border py-5 px-4 mx-3 my-5 bg-white shadow shadows-sm rounded">
+            <h3 class="text-center fw-bold">Páginas WEB</h3>
+            <hr>
+            <span class="fs-5 text-justify">Obten una pagina web personalizada para tener presencia en internet.</span>
+            <ul class="mt-5 comics_sans">
+                <li class=" fs-4"> Blogs. </li>
+                <li class=" fs-4"> Información de tu negocio.</li>
+                <li class=" fs-4"> Información acerca de ti.</li>
+                <li class=" fs-4"> Proyectos escolares.</li>
+                <li class=" fs-4"> Etc.</li>
+            </ul>
+        </div>
+
+
+
+        <div class="col-sm-12 col-md-12  col-lg-3 border py-5  px-4 mx-3 my-5 bg-white shadow shadows-sm rounded">
+            <h3 class="text-center fw-bold">Aplicaciones WEB</h3>
+            <hr>
+            <span class="fs-5 text-justify">Automatiza los procesos de tu negocio.</span>
+            <ul class="mt-3 arvo-regular">
+                <li class=" fs-3">Gestiona a tus clientes y proveedores.</li>
+                <li class=" fs-3">Gestiona tus ventas.</li>
+                <li class=" fs-3">Automatiza la gestion de tu información.</li>
+            </ul>
+        </div>
+
+
+
     </div>
+
+
+
+
+
+
+    <div class="row banner2 justify-content-around text-white py-5 my-1 animacion1">
+
+        <div class="col-sm-12 col-md-12 col-lg-7 p-5 bolder text-sm-center text-lg-start text-md-start bg-dark box hidden animate__animated animate__backInRight" style="--bs-bg-opacity: .7;">
+            <span class="fw-bolder text-with-border">SERVICIO DE SOPORTE TÉCNICO</span><br>
+        </div>
+    
+        <div class="col-sm-12 col-md-12 col-lg-1 bolder text-sm-center text-lg-start text-md-start mt-5 p-5">
+            <button class="btn btn-info btn-lg p-5 d-flex fw-bold animate__animated fs-2 animate__rubberBand mt-3">
+                <i class="fa fa-money"></i>
+                Cotizar
+            </button>
+        </div>
+    
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 </div> 
--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <title>@yield('title')</title>
 </head>
 <body>
@@ -16,5 +18,39 @@
 
     @yield('scripts')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+
+        window.addEventListener('scroll', function() {
+            const elemento = document.getElementById('animacion1');
+            const posicion = elemento.getBoundingClientRect().top;
+            const alturaPantalla = window.innerHeight;
+
+            if(posicion < alturaPantalla) {
+                elemento.classList.remove('hidden');
+            }
+        });
+
+
+    // Configuración del observador de intersección
+    document.addEventListener('DOMContentLoaded', function() {
+        let observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                // Si el elemento es visible en la pantalla
+                if (entry.isIntersecting) {
+                    // Agrega la clase que activa la animación y remueve 'hidden'
+                    entry.target.classList.add('animate__backInRight');
+                    entry.target.classList.remove('hidden');
+                }
+            });
+        }, { threshold: 0.5 }); // El 50% del elemento debe estar visible
+
+        // Selecciona el elemento que deseas animar
+        const animacionElement = document.querySelector('.animacion2 .box');
+        if (animacionElement) {
+            observer.observe(animacionElement);
+        }
+    });
+</script>
+
 </body>
 </html>
